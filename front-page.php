1b9912fefdad2d469fa8ff8c38e4785a53222f05
lcarranco <larry@carrancomarketing.com>
2016-01-26 02:09:07 -0600

    Added text to the blue section. Added images to the blue section.

--- front-page.php
+++ front-page.php
@@ -3,23 +3,72 @@
 	<div class="container">
 		<div class="col-md-12 row greenContainer">
 			<div class="col-md-8 col-md-offset-3 hyooHeader">
-				<img class="blueDiamond" src="<?php echo get_template_directory_uri(); ?>/images/svg/HYOO-Header.svg" alt="Hyoo Header" />
+				<img class="blueDiamond" src="<?php echo get_template_directory_uri(); ?>/images/svg/HYOO-Header.svg" alt="Hyoo Header" >
 				<a href="" class="chatLink">Interested? Let's chat.</a>
 			</div>
 			<div class="col-md-1 text-right hyooLogoContainer">
-				<img class="hyooLogo" src="<?php echo get_template_directory_uri(); ?>/images/hyoo-logo.png" alt="Hyoo Logo" />
+				<img class="hyooLogo" src="<?php echo get_template_directory_uri(); ?>/images/hyoo-logo.png" alt="Hyoo Logo" >
 			</div>
 		</div>
 	</div>
 </div>
 <div class="blueGreenGradient">
 	<div class="container">
-		<div class="col-md-12 row">
-			<div class="col-md-11 col-md-offset-1 row">
-				<p>Abilities</p>
+		<div class="col-md-11 col-md-offset-1">
+			<p>Abilities</p>
+		</div>
+		<div class="col-md-12">
+			<div class="col-md-5 col-md-offset-1 row sectionTitle">
+				<h1>WE CAN DO A LOT + SOME</h1>
 			</div>
-			<div class="col-md-5 col-md-offset-1 row">
-				<h1>WE CAN DO A LOT + SOME boobies</h1>
+		</div>
+		<div class="col-md-12 brandingAndDesignArea">
+			<div class="col-md-1 col-md-offset-1">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/branding-icon.png" alt="Branding Icon" >
+			</div>
+			<div class="col-md-4">
+				<h2>Branding</h2>
+				<p>We will take you from A - Z and then to !$%^ Yeah! No joke we know how to make your brand look
+				its best.</p>
+			</div>
+			<div class="col-md-1 col-md-offset-1">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/design-icon.png" alt="Design Icon" >
+			</div>
+			<div class="col-md-4">
+				<h2>Design</h2>
+				<p>We’ve been called everything from Design Demi-Gods to Sages of Symmetry, but one thing is for sure; we’ll have all your print and digital design needs taken care of.</p>
+			</div>
+		</div>
+		<div class="col-md-12 devlopmentAndPhotographyArea">
+			<div class="col-md-1 col-md-offset-1">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/development-icon.png" alt="Development Icon" >
+			</div>
+			<div class="col-md-4">
+				<h2>Development</h2>
+				<p>Code can be tough and confusing for the average Joe, no worries we've got your back-end (and front-end development covered).</p>
+			</div>
+			<div class="col-md-1 col-md-offset-1">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/photography-icon.png" alt="Photography Icon" >
+			</div>
+			<div class="col-md-4">
+				<h2>Photography</h2>
+				<p>Nothings worse than using cheesy stock photography. Okay well some things are worse, but it’s always better to use authentic imagery that actualy represents your brand.</p>
+			</div>
+		</div>
+		<div class="col-md-12 motionAndSocialArea">
+			<div class="col-md-1 col-md-offset-1">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/motion-icon.png" alt="Motion Icon" >
+			</div>
+			<div class="col-md-4">
+				<h2>Motion</h2>
+				<p>You know what makes pretty pictures more interesting? Pretty pictures that move. Luckly for you we are supergood at everything motion.</p>
+			</div>
+			<div class="col-md-1 col-md-offset-1">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/social-icon.png" alt="Social Icon" >
+			</div>
+			<div class="col-md-4">
+				<h2>Social Strategy</h2>
+				<p>Most companies don’t have the time or energy to keep up with the gluttony of social media. No worries, we are social superstars and can help you in your quest for stockpiling friends.</p>
 			</div>
 		</div>
 	</div>
