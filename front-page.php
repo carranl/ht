adb55a93985c6aee67a09bf3521d41e9b541cdac
lcarranco <larry@carrancomarketing.com>
2016-01-27 16:06:08 -0600

    greenBlue Gradient Section: Fixed the padding/margin on all text sections. Added correct color to Abilities text. Added correct color to weCanText. Added correct color to services area. Took a while to figure out how to apply the solid green line next to the Abilities text.
    bluePinkGradient Section: Added correct color to contactText. Added correct color to the rest of the section text. Added correct color to email text. Fixed the padding/margin issue.
    
    Tried to figure out how to apply the large pink ellipse to the bluePinkGradient section area, but could not figure it out. Will ask Jose for help.

--- front-page.php
+++ front-page.php
@@ -14,76 +14,87 @@
 </div>
 <div class="blueGreenGradient">
 	<div class="container">
-		<div class="col-md-11 col-md-offset-1">
-			<p>Abilities ---------</p>
-		</div>
-		<div class="col-md-5 col-md-offset-1 row sectionTitle">
-			<h1>WE CAN DO A LOT + SOME</h1>
-		</div>
-		<div class="col-md-12 brandingAndDesignArea">
-			<div class="col-md-1 col-md-offset-1">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/branding-icon.png" alt="Branding Icon" >
+		<div class="row">
+			<div class="col-md-12">
+				<p class="col-md-2 col-md-offset-1 abilitiesText">
+					Abilities
+				</p>
 			</div>
-			<div class="col-md-4">
-				<h2>Branding</h2>
-				<p>We will take you from A - Z and then to !$%^ Yeah! No joke we know how to make your brand look
-				its best.</p>
+			<div class="col-md-5 col-md-offset-1 row weCanText">
+				<h1>WE CAN DO A LOT + SOME</h1>
 			</div>
-			<div class="col-md-1 col-md-offset-1">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/design-icon.png" alt="Design Icon" >
+			<div class="col-md-12 row brandingAndDesignArea">
+				<div class="col-md-1 col-md-offset-1">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/branding-icon.png" alt="Branding Icon" >
+				</div>
+				<div class="col-md-4">
+					<h2>Branding</h2>
+					<p>We will take you from A - Z and then to !$%^ Yeah! No joke we know how to make your brand look
+					its best.</p>
+				</div>
+				<div class="col-md-1 col-md-offset-1">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/design-icon.png" alt="Design Icon" >
+				</div>
+				<div class="col-md-4">
+					<h2>Design</h2>
+					<p>We’ve been called everything from Design Demi-Gods to Sages of Symmetry, but one thing is for sure; we’ll have all your print and digital design needs taken care of.</p>
+				</div>
 			</div>
-			<div class="col-md-4">
-				<h2>Design</h2>
-				<p>We’ve been called everything from Design Demi-Gods to Sages of Symmetry, but one thing is for sure; we’ll have all your print and digital design needs taken care of.</p>
+			<div class="col-md-12 row devlopmentAndPhotographyArea">
+				<div class="col-md-1 col-md-offset-1">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/development-icon.png" alt="Development Icon" >
+				</div>
+				<div class="col-md-4">
+					<h2>Development</h2>
+					<p>Code can be tough and confusing for the average Joe, no worries we've got your back-end (and front-end development covered).</p>
+				</div>
+				<div class="col-md-1 col-md-offset-1">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/photography-icon.png" alt="Photography Icon" >
+				</div>
+				<div class="col-md-4">
+					<h2>Photography</h2>
+					<p>Nothings worse than using cheesy stock photography. Okay well some things are worse, but it’s always better to use authentic imagery that actualy represents your brand.</p>
+				</div>
 			</div>
-		</div>
-		<div class="col-md-12 devlopmentAndPhotographyArea">
-			<div class="col-md-1 col-md-offset-1">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/development-icon.png" alt="Development Icon" >
-			</div>
-			<div class="col-md-4">
-				<h2>Development</h2>
-				<p>Code can be tough and confusing for the average Joe, no worries we've got your back-end (and front-end development covered).</p>
-			</div>
-			<div class="col-md-1 col-md-offset-1">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/photography-icon.png" alt="Photography Icon" >
-			</div>
-			<div class="col-md-4">
-				<h2>Photography</h2>
-				<p>Nothings worse than using cheesy stock photography. Okay well some things are worse, but it’s always better to use authentic imagery that actualy represents your brand.</p>
-			</div>
-		</div>
-		<div class="col-md-12 motionAndSocialArea">
-			<div class="col-md-1 col-md-offset-1">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/motion-icon.png" alt="Motion Icon" >
-			</div>
-			<div class="col-md-4">
-				<h2>Motion</h2>
-				<p>You know what makes pretty pictures more interesting? Pretty pictures that move. Luckly for you we are supergood at everything motion.</p>
-			</div>
-			<div class="col-md-1 col-md-offset-1">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/social-icon.png" alt="Social Icon" >
-			</div>
-			<div class="col-md-4">
-				<h2>Social Strategy</h2>
-				<p>Most companies don’t have the time or energy to keep up with the gluttony of social media. No worries, we are social superstars and can help you in your quest for stockpiling friends.</p>
+			<div class="col-md-12 row motionAndSocialArea">
+				<div class="col-md-1 col-md-offset-1">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/motion-icon.png" alt="Motion Icon" >
+				</div>
+				<div class="col-md-4">
+					<h2>Motion</h2>
+					<p>You know what makes pretty pictures more interesting? Pretty pictures that move. Luckly for you we are supergood at everything motion.</p>
+				</div>
+				<div class="col-md-1 col-md-offset-1">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/social-icon.png" alt="Social Icon" >
+				</div>
+				<div class="col-md-4">
+					<h2>Social Strategy</h2>
+					<p>Most companies don’t have the time or energy to keep up with the gluttony of social media. No worries, we are social superstars and can help you in your quest for stockpiling friends.</p>
+				</div>
 			</div>
 		</div>
 	</div>
 </div>
 <div class="bluePinkGradient">
 	<div class="container">
-		<div class="col-md-11 col-md-offset-1">
-			<p>Contact ---------</p>
-		</div>
-		<div class="col-md-4 col-md-offset-1">
-			<h1>WE'D LOVE TO TALK</h1>
-		</div>
-		<div class="col-md-9 col-md-offset-1">
-			<h1>Let's get this conversation started, hit us up at <a href="">hey@hyoo.co</a></h1>
-		</div>
-		<div class="col-md-9 col-md-offset-1">
-			<p>Seriously though, we’ll send you examples of our work, chat about ideas and become fast friends.</p>
+		<div class="row">
+			<div class="col-md-12">
+				<p class="col-md-2 col-md-offset-1 contactText">
+					Contact
+				</p>
+			</div>
+			<div class="col-md-4 col-md-offset-1 talkSection">
+				<h1>WE'D LOVE TO TALK</h1>
+			</div>
+			<div class="col-md-4 col-md-offset-3">
+				<img class="largeEllipsePink" src="<?php echo get_template_directory_uri(); ?>/images/svg/Ellipse-SoftLight-filter.svg" alt="Large Ellipse Pink" >
+			</div>
+			<div class="col-md-9 col-md-offset-1 conversationSection">
+				<h1>Let's get this conversation started, hit us up at <a href="">hey@hyoo.co</a></h1>
+			</div>
+			<div class="col-md-9 col-md-offset-1 exampleSection">
+				<p>Seriously though, we’ll send you examples of our work, chat about ideas and become fast friends.</p>
+			</div>
 		</div>
 	</div>
 </div>
