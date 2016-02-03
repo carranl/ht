138decc963080c141c1d57049e9add6ed1cd9b4b
lcarranco <larry@carrancomarketing.com>
2016-02-02 21:30:04 -0600

    ******>= 1199px******
    Corrected the error of coding the blue diamond before the logo
    
    Changed to columns in the green container area from md to xs
    
    Adjusted blue slash height, width, and margin
    
    Adjusted Large Pink Ellipse column
    
    Fixed the line break that was breaking the container
    
    Created a special media break at 1145px for the Large Pink Ellipse only
    
     ****1199px to 991px****
    
    Adjusted The blue diamon width
    
    Adjusted the blue slash width
    
     ****991px to 767px****
    
    Adjusted individually all 6 services margin top
    
    Adjusted with of the logo
    
    Adjusted the width  of the blue diamond
    
    Adjusted the width, top, and left of the blue slash
    
    Adjusted the width and margins of the Large Pink Ellipse

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
   <footer>
   	<div class="container">
   		<div class="row copyright">
-  			<div class="col-md-2 col-md-offset-1">
+  			<div class="col-xs-3 col-xs-offset-1">
           <p>Copyright <?php echo date("Y")?> &copy; HYOO.CO</p>
         </div>
-        <div class="col-md-1 text-center">
+        <div class="col-xs-1 text-center">
           <p>|</p>
         </div>
-        <div class="col-md-3">
+        <div class="col-xs-3">
           <p>In partnership with <a href="http://www.carrancomarketing.com" target="blank">Carranco Marketing</a></p>
         </div>
       </div>
--- front-page.php
+++ front-page.php
@@ -2,11 +2,11 @@
 <div class="greenBackground">
 	<div class="container">
 		<div class="col-md-12 row greenContainer">
-			<div class="col-md-8 col-md-offset-3 hyooHeader">
-				<img class="blueDiamond" src="<?php echo get_template_directory_uri(); ?>/images/svg/HYOO-Header.svg" alt="Hyoo Header" >
-			</div>
-			<div class="col-md-1 text-right hyooLogoContainer">
+			<div class="col-xs-1 col-xs-offset-11 text-center hyooLogoContainer">
 				<img class="hyooLogo" src="<?php echo get_template_directory_uri(); ?>/images/hyoo-logo.png" alt="Hyoo Logo" >
+			</div>
+			<div class="col-xs-8 col-xs-offset-3 hyooHeader">
+				<img class="blueDiamond" src="<?php echo get_template_directory_uri(); ?>/images/svg/HYOO-Header.svg" alt="Hyoo Header" >
 			</div>
 		</div>
 		<div class="col-md-2 col-md-offset-6 chatLinkColumn">
@@ -23,57 +23,57 @@
 					Abilities
 				</p>
 			</div>
-			<div class="col-md-11 col-md-offset-1 row weCanText">
+			<div class="col-md-11 col-md-offset-1 weCanText">
 				<h1>WE CAN DO</h1>
 			</div>
-			<div class="col-md-11 col-md-offset-1 row weCanText weCanTextSecond">
+			<div class="col-md-11 col-md-offset-1 weCanText weCanTextSecond">
 				<h1>A LOT <span>+</span> SOME</h1>
 			</div>
 			<div class="col-md-12 row brandingAndDesignArea">
 				<div class="col-md-1 col-md-offset-1">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/branding-icon.png" alt="Branding Icon" >
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-4 brandingTextColumn">
 					<h2>Branding</h2>
 					<p>We will take you from A - Z and then to !$%^ Yeah! No joke we know how to make your brand look
 					its best.</p>
 				</div>
-				<div class="col-md-1 col-md-offset-1">
+				<div class="col-md-1 col-md-offset-1 designIconColumn">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/design-icon.png" alt="Design Icon" >
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-4 designTextColomn">
 					<h2>Design</h2>
 					<p>We’ve been called everything from Design Demi-Gods to Sages of Symmetry, but one thing is for sure; we’ll have all your print and digital design needs taken care of.</p>
 				</div>
 			</div>
 			<div class="col-md-12 row devlopmentAndPhotographyArea">
-				<div class="col-md-1 col-md-offset-1">
+				<div class="col-md-1 col-md-offset-1 developmentIconColumn">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/development-icon.png" alt="Development Icon" >
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-4 developmentTextColumn">
 					<h2>Development</h2>
 					<p>Code can be tough and confusing for the average Joe, no worries we've got your back-end (and front-end development covered).</p>
 				</div>
-				<div class="col-md-1 col-md-offset-1">
+				<div class="col-md-1 col-md-offset-1 photographyIconColumn">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/photography-icon.png" alt="Photography Icon" >
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-4 photographyTextColumn">
 					<h2>Photography</h2>
 					<p>Nothings worse than using cheesy stock photography. Okay well some things are worse, but it’s always better to use authentic imagery that actualy represents your brand.</p>
 				</div>
 			</div>
 			<div class="col-md-12 row motionAndSocialArea">
-				<div class="col-md-1 col-md-offset-1">
+				<div class="col-md-1 col-md-offset-1 motionIconColumn">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/motion-icon.png" alt="Motion Icon" >
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-4 motionTextColumn">
 					<h2>Motion</h2>
 					<p>You know what makes pretty pictures more interesting? Pretty pictures that move. Luckly for you we are supergood at everything motion.</p>
 				</div>
-				<div class="col-md-1 col-md-offset-1">
+				<div class="col-md-1 col-md-offset-1 socialIconColumn">
 					<img src="<?php echo get_template_directory_uri(); ?>/images/social-icon.png" alt="Social Icon" >
 				</div>
-				<div class="col-md-4">
+				<div class="col-md-4 socialTextColumn">
 					<h2>Social Strategy</h2>
 					<p>Most companies don’t have the time or energy to keep up with the gluttony of social media. No worries, we are social superstars and can help you in your quest for stockpiling friends.</p>
 				</div>
@@ -92,10 +92,10 @@
 			<div class="col-md-4 col-md-offset-1 talkSection">
 				<h1>WE'D LOVE TO TALK</h1>
 			</div>
-			<div class="col-md-4 col-md-offset-3 largeEllipsePinkColumn">
+			<div class="col-md-4 col-md-offset-2 largeEllipsePinkColumn">
 				<img class="largeEllipsePink" src="<?php echo get_template_directory_uri(); ?>/images/svg/Ellipse-SoftLight-Filter.svg" alt="Large Ellipse Pink" >
 			</div>
-			<div class="col-md-10 row">
+			<div class="col-xs-10 row">
 				<img class="dottedLine" src="<?php echo get_template_directory_uri(); ?>/images/lineBreakFull.png" alt="Dotted Line" >
 			</div>
 			<div class="col-md-9 col-md-offset-1 conversationSection">
